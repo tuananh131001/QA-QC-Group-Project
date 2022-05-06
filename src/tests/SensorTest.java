e4396256bab425f07271d4c4049933bfed284e11
s <s3864077@rmit.edu.vn>
2022-05-06 20:56:43 +0700

    test dc r

--- src/tests/SensorTest.java
+++ src/tests/SensorTest.java
@@ -1,13 +1,34 @@
 package tests;
 
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import support.Sensor;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
 public class SensorTest {
-    @ParameterizedTest
-    @ValueSource(strings = {"Alex","AQI"})
-    void testGetUserName(String input){
+    @Parameterized.Parameter(0)
+    public String name;
 
+    @Parameterized.Parameter(1)
+    public String type;
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        Object[][] data = new Object[][] { { "Jack","AQI" }, {  "Jack","Location" } };
+        return Arrays.asList(data);
+    }
+
+
+    @Test
+    public void testGetUserName(){
+        Sensor sensor = new Sensor(name,type);
+        assertEquals(sensor.getUsername(),"Jack");
     }
 }
