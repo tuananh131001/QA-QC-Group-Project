b7c13116e19ae39c7b8f3760966461d9acd1470b
s <s3864077@rmit.edu.vn>
2022-05-10 14:49:49 +0700

    gud

--- src/tests/Sensor/sensor0Seconds.java
+++ src/tests/Sensor/sensor0Seconds.java
@@ -9,11 +9,10 @@ import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.LinkedHashMap;
-
+import java.util.Objects;
 import static org.junit.Assert.assertEquals;
-
 @RunWith(Parameterized.class)
-public class sensorTestCurrentValueAndSecond {
+public class sensor0Seconds {
     @Parameterized.Parameter(0)
     public String name;
     @Parameterized.Parameter(1)
@@ -28,28 +27,36 @@ public class sensorTestCurrentValueAndSecond {
     @Parameterized.Parameters
     public static Collection<Object[]> data() {
         Object[][] data = new Object[][]{
-                {"Jack", "AQI", 10,"200", 14},
-                {"Jack", "Location", 1,"A", 0},
-                {"Jack", "Temperature",3, "10", 4},
-                {"David", "AQI",10, "200", 14},
-                {"David", "Location",1, "A", 0},
-                {"David", "Temperature",3, "10", 4}};
+                {"Jack", "AQI", 0,"200", 10},
+                {"Jack", "Location", 0,"A", 14},
+                {"Jack", "Temperature",0, "10", 2},
+                {"David", "AQI",0, "200", 10},
+                {"David", "Location",0, "A", 14},
+                {"David", "Temperature",0, "10", 2}};
         return Arrays.asList(data);
     }
 
     @Test
-    public void testGetCurrent() {
+    public void testGetCurrent0() throws NoSuchFieldException, IllegalAccessException {
         Sensor sensor = new Sensor(name, type);
-        assertEquals(sensor.getCurrentValue(), expected);
-    }
-    @Test
-    public void testGetSec() throws NoSuchFieldException, IllegalAccessException {
-        Sensor sensor = new Sensor(name, type);
-        sensor.getCurrentValue();
         Field secondField = Sensor.class.getDeclaredField("seconds");
         secondField.setAccessible(true);
         Integer fieldValue = (Integer) secondField.get(sensor);
-        assertEquals(fieldValue, expectedSeconds);
+        while((Integer) secondField.get(sensor) > 1){
+            sensor.getCurrentValue();
+        }
+        assertEquals(sensor.getCurrentValue(), expected);
     }
-
+    @Test
+    public void testGetSec0() throws NoSuchFieldException, IllegalAccessException {
+        Sensor sensor = new Sensor(name, type);
+        Field secondField = Sensor.class.getDeclaredField("seconds");
+        secondField.setAccessible(true);
+        Integer fieldValue = (Integer) secondField.get(sensor);
+        while((Integer) secondField.get(sensor) != 0){
+            sensor.getCurrentValue();
+        }
+        sensor.getCurrentValue();
+        assertEquals((Integer) secondField.get(sensor) , expectedSeconds);
+    }
 }
